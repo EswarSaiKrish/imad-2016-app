bc54ec3cd1ef76e9161f69edc439ee0d160ab814
EswarSaiKrish <eswar.gundabolu@gmail.com>
2016-10-27 18:10:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
-console.log('Loaded!');
+ var button=document.getElementById("busnumber");
+ 
+ 
+ button.onclick=function loadMap() {
+			
+   var mapOptions = {
+               center:new google.maps.LatLng(17.609993, 83.221436), 
+               zoom:12, 
+               mapTypeId:google.maps.MapTypeId.ROADMAP
+            };
+				
+            var map = new google.maps.Map(document.getElementById("map"),mapOptions);
+         }
+			
+      
+      busnumber.innerHTML=button;
+   
